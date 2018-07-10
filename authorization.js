f19ce0caa8189383412e6910f77148d039437fb8
agcollins <agcollins@utexas.edu>
2018-07-09 20:35:44 -0500

    headers are now uppercase

--- authorization.js
+++ authorization.js
@@ -2,8 +2,8 @@ const axios = require('axios')
 const client = axios.create({
     baseUrl: '<endpoint-url>',
     headers: {
-        authorization: '<api-key>',
-        accept: 'application/vnd.api+json'
+        Authorization: '<api-key>',
+        Accept: 'application/vnd.api+json',
     }
 })
 
